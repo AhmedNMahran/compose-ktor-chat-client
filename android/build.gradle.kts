b5ea6863b91a4e241f5d3da99f25b92b4a9fd583
Ahmed Nabil <ahmed.mahran@vodafone.com>
2022-09-16 02:44:40 +0200

    add coil android dependency

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+    implementation("io.coil-kt:coil-compose:2.2.1")
 }
 
 android {
