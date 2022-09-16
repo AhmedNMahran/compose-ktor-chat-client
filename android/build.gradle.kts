65381ad0986fec66e4760ecc41102fd20837fce1
Ahmed Nabil <ahmed.mahran@vodafone.com>
2022-09-16 02:43:51 +0200

    update compile and min sdk to 33

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,11 +19,11 @@ dependencies {
 }
 
 android {
-    compileSdkVersion(31)
+    compileSdkVersion(33)
     defaultConfig {
         applicationId = "com.github.ahmednmahran.android"
         minSdkVersion(24)
-        targetSdkVersion(31)
+        targetSdkVersion(33)
         versionCode = 1
         versionName = "1.0-SNAPSHOT"
     }
